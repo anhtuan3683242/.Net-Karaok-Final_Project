659cd873005cd89f4ea0df2a4293b031475fcd9a
= <=>
2020-11-19 21:15:31 +0700

    oke babe

---
+++ QuanLyKaraoke/QuanLyKaraoke/Migrations/202011191413316_init.cs
@@ -0,0 +1,18 @@
+﻿namespace QuanLyKaraoke.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Booking", "EndTime", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Booking", "EndTime");
+        }
+    }
+}
