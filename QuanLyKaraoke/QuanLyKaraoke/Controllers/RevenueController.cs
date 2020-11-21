2c96dd63f3131c5ab4e4c3109a3a38818765efa1
anhtuan3683242 <55082542+anhtuan3683242@users.noreply.github.com>
2020-11-21 09:33:12 +0700

    chạy thử

--- QuanLyKaraoke/QuanLyKaraoke/Controllers/RevenueController.cs
+++ QuanLyKaraoke/QuanLyKaraoke/Controllers/RevenueController.cs
@@ -13,62 +13,72 @@ namespace QuanLyKaraoke.Controllers
         // GET: Revenue
         public ActionResult Revenue()
         {
-
-            var revenueDao = new RevenueDao();
-
-            //var labels = new List<string>() { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Sep", "Oct", "Nov", "Dec" };
-            var labels = new List<string>() { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec" };
-            var labels1 = new List<string>() { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
-
-            var labelsPie = new List<string>() { "Standard", "VIP", "Luxury" };
-            //test numguest
-
-            //var data = new List<int>() { 500, 320, 132, 346, 567, 621, 712, 811, 818, 902, 985 };
-
-            var data = new List<int>();
-            var data1 = new List<int>();
-
-            var dataPie = new List<int>();
-
-
-            for (int x = 0; x < 12; x++)
+            if (Session["S_ID"] != null && Session["Role"].ToString() != "Staff")
             {
-                data1.Add(revenueDao.GetTotal(x + 1));
-            }
+                var revenueDao = new RevenueDao();
+
+                //var labels = new List<string>() { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Sep", "Oct", "Nov", "Dec" };
+                var labels = new List<string>() { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec" };
+                var labels1 = new List<string>() { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+                var labelsPie = new List<string>() { "Standard", "VIP", "Luxury" };
+                //test numguest
+
+                //var data = new List<int>() { 500, 320, 132, 346, 567, 621, 712, 811, 818, 902, 985 };
+
+                var data = new List<int>();
+                var data1 = new List<int>();
+
+                var dataPie = new List<int>();
+
+
+                for (int x = 0; x < 12; x++)
+                {
+                    data1.Add(revenueDao.GetTotal(x + 1));
+                }
             
-            var doanhthu = new BarChart()
-            {
-                Labels = labels,
+                var doanhthu = new BarChart()
+                {
+                    Labels = labels,
 
-                Data = revenueDao.GetNumBook(),
-                max = revenueDao.GetMax(),
+                    Data = revenueDao.GetNumBook(),
+                    max = revenueDao.GetMax(),
 
                 
-            };
+                };
 
-            var totaldt = new LineChart()
+                var totaldt = new LineChart()
+                {
+                    Labels1 = labels1,
+                    Data1 = data1
+                };
+
+                var typeRoom = new PieChart()
+                {
+                    LabelsPie = labelsPie,
+                    DataPie = revenueDao.GetTRoom(),
+
+                };
+
+                var model = new RevenueModel()
+                {
+                    DoanhThuChart = doanhthu,
+                    TotalDT = totaldt,
+                    TypeRoom = typeRoom
+                };
+
+
+                return View(model);
+            }
+            else if (Session["S_ID"] != null)
             {
-                Labels1 = labels1,
-                Data1 = data1
-            };
-
-            var typeRoom = new PieChart()
+                TempData["ErrorMessage"] = "You are not authorized to access this page";
+                return RedirectToAction("Admin_index", "Home");
+            }
+            else
             {
-                LabelsPie = labelsPie,
-                DataPie = revenueDao.GetTRoom(),
-
-            };
-
-            var model = new RevenueModel()
-            {
-                DoanhThuChart = doanhthu,
-                TotalDT = totaldt,
-                TypeRoom = typeRoom
-            };
-
-
-            return View(model);
-
+                return RedirectToAction("Login", "Home");
+            }
         }
 
     }
